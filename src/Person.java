7c5631bb3ebb8f2c9ac75c228d7f18c4fb767727
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-09-04 14:39:37 +0530

    Making geter method of name, age and gender.

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,14 @@ public class Person {
     private String name;
     private int age;
     private char gender;
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public char getGender() {
+        return gender;
+    }
 }
